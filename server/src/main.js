89dfc3db8cc4fe90651a56d5a7be107cc2b99a96
Joshua Sanderson <joshua@thisisthesandersons.co.uk>
2021-01-08 11:20:30 +0000

    Deleted everything lol

--- server/src/main.js
+++
@@ -1,8 +0,0 @@
-import { initExpress } from './libs/express_no-save'
-import { initStream } from './libs/nms'
-import log from './utils/log'
-
-log('info', 'Skeleton', 'Skeleton Loaded')
-
-initExpress()
-initStream()
